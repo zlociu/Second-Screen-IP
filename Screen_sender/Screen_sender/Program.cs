c7dece2980a5d59f33cee3ba4e2e98d13d738048
zlociu <zlociu@onet.eu>
2020-04-23 17:11:27 +0200

    first code

---
+++ Screen_sender/Screen_sender/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace Screen_sender
+{
+    class Program
+    {
+
+        static int ipPort;
+        static string ipAddr;
+
+        static bool connectionEnd = false;
+
+        static async Task sendScreenData(int delay_ms)
+        {
+            ScreenCapture screen = new ScreenCapture(1.25f);
+            TcpClient sender = new TcpClient(new IPEndPoint(IPAddress.Parse(ipAddr), ipPort));
+            while(!connectionEnd)
+            {
+                byte[] data = screen.captureWithMouse(1.25f);
+                await sender.GetStream().WriteAsync(null, 0, 0);
+                Thread.Sleep(17);
+            }
+        }
+
+        static Task serverControl()
+        {
+            Console.WriteLine("jkk");
+            return null;
+        }
+
+        static void Main(string[] args)
+        {
+            ipPort = 11309;
+            ipAddr = "127.0.0.1";
+            connectionEnd = false;
+            ScreenCapture sc = new ScreenCapture(1.25f);
+            byte[] array = sc.captureWithMouse();
+            //Task t1 = sendScreenData();
+            //Task t2 = serverControl();
+            //t1.Wait();
+            //t2.Wait();
+        }
+    }
+}
---
+++ Screen_sender/Screen_sender/ScreenCapture.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace Screen_sender
+{
+    class ScreenCapture
+    {
+        private float DPI; 
+
+        public ScreenCapture(float DPI)
+        {
+            this.DPI = DPI;
+        }
+        private ImageCodecInfo GetEncoderInfo(String mimeType)
+        {
+            int j;
+            ImageCodecInfo[] encoders;
+            encoders = ImageCodecInfo.GetImageEncoders();
+            for (j = 0; j < encoders.Length; ++j)
+            {
+                if (encoders[j].MimeType == mimeType)
+                    return encoders[j];
+            }
+            return null;
+        }
+
+        public void saveScreenshot()
+        {
+            Rectangle scrBounds = Rectangle.FromLTRB(Screen.PrimaryScreen.Bounds.Left, Screen.PrimaryScreen.Bounds.Top, (int)(Screen.PrimaryScreen.Bounds.Right * DPI), (int)(Screen.PrimaryScreen.Bounds.Bottom * DPI));
+            Bitmap desktopBMP = new Bitmap(scrBounds.Width, scrBounds.Height);
+
+            // --------------< save image to file >-----------------
+            ImageCodecInfo imageCodecInfo = GetEncoderInfo("image/jpeg");
+            System.Drawing.Imaging.Encoder encoder = System.Drawing.Imaging.Encoder.Quality;
+            EncoderParameter encoderParameter = new EncoderParameter(encoder, 100L);
+            EncoderParameters encoderParameters = new EncoderParameters(1);
+            encoderParameters.Param[0] = encoderParameter;
+            using (Graphics g = Graphics.FromImage(desktopBMP))
+            {
+                g.CopyFromScreen(scrBounds.Location, Point.Empty, scrBounds.Size);
+            }
+            desktopBMP.Save("screen_" + DateTime.Now.Year + '-' + DateTime.Now.DayOfYear + '-' + DateTime.Now.TimeOfDay + ".jpg", imageCodecInfo, encoderParameters);
+        }
+
+        public void saveScreenshot(Bitmap bmp)
+        {
+            ImageCodecInfo imageCodecInfo = GetEncoderInfo("image/jpeg");
+            System.Drawing.Imaging.Encoder encoder = System.Drawing.Imaging.Encoder.Quality;
+            EncoderParameter encoderParameter = new EncoderParameter(encoder, 100L);
+            EncoderParameters encoderParameters = new EncoderParameters(1);
+            encoderParameters.Param[0] = encoderParameter;
+            bmp.Save("screen_" + DateTime.Now.Year + '-' + DateTime.Now.DayOfYear + '-' + DateTime.Now.TimeOfDay + ".jpg", imageCodecInfo, encoderParameters);
+        }
+
+        public byte[] captureWithMouse()
+        {
+            var allBounds = Screen.AllScreens.Select(s => s.Bounds).ToArray();
+            Cursor cur = Cursor.Current;
+            Rectangle scrBounds = Rectangle.FromLTRB(allBounds.Min(b => b.Left), allBounds.Min(b => b.Top), (int) (allBounds.Max(b => b.Right) * DPI), (int) (allBounds.Max(b => b.Bottom) * DPI));
+            //Console.WriteLine(allBounds.Length);
+            Rectangle curBounds = new Rectangle(Cursor.Position, cur.Size);
+            Bitmap desktopBMP = new Bitmap(scrBounds.Width, scrBounds.Height);
+            Bitmap cursorBMP = new Bitmap(cur.Size.Width, cur.Size.Height);
+
+            // --------------< save image to file >-----------------
+            ImageCodecInfo imageCodecInfo = GetEncoderInfo("image/jpeg");
+            System.Drawing.Imaging.Encoder encoder = System.Drawing.Imaging.Encoder.Quality;
+            EncoderParameter encoderParameter = new EncoderParameter(encoder, 100L);
+            EncoderParameters encoderParameters = new EncoderParameters(1);
+            encoderParameters.Param[0] = encoderParameter;
+            using (Graphics g = Graphics.FromImage(desktopBMP))
+            {
+
+                g.CopyFromScreen(scrBounds.Location, Point.Empty, scrBounds.Size);
+                cur.Draw(g,curBounds);      
+            }
+            desktopBMP.Save("screenShot.jpg", imageCodecInfo, encoderParameters);
+
+            ImageConverter conv = new ImageConverter();
+        
+            return (byte[])conv.ConvertTo(desktopBMP, typeof(byte[]));
+
+        }
+    }
+}
