7eec3ae1de582226d0c12e6eb2460398c03da824
zlociu <zlociu@onet.eu>
2020-04-24 13:05:36 +0200

    new functions

--- Screen_sender/Screen_sender/Program.cs
+++ Screen_sender/Screen_sender/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Net;
 using System.Threading;
+using System.IO.Compression;
 
 namespace Screen_sender
 {
@@ -20,19 +21,65 @@ namespace Screen_sender
         static async Task sendScreenData(int delay_ms)
         {
             ScreenCapture screen = new ScreenCapture(1.25f);
-            TcpClient sender = new TcpClient(new IPEndPoint(IPAddress.Parse(ipAddr), ipPort));
-            while(!connectionEnd)
+            TcpClient sender = new TcpClient(new IPEndPoint(IPAddress.Parse(ipAddr), 0));
+            GZipStream compressionStream = new GZipStream(sender.GetStream(), CompressionMode.Compress);
+            while (!connectionEnd)
             {
-                byte[] data = screen.captureWithMouse(1.25f);
-                await sender.GetStream().WriteAsync(null, 0, 0);
-                Thread.Sleep(17);
+                byte[] data = screen.captureWithMouse();
+                await compressionStream.WriteAsync(data, 0, data.Length);
+                Thread.Sleep(delay_ms);
             }
         }
 
         static Task serverControl()
         {
             Console.WriteLine("jkk");
-            return null;
+            while (true)
+            {
+                string command = Console.ReadLine();
+                var tab = command.Split(new char[]{' '}, 2);
+                int len = tab.Length;
+                switch(tab[0])
+                {
+                    case "start":
+                        {
+                            ipAddr = tab[1];
+                            ipPort = int.Parse(tab[2]);
+
+                        }
+                        break;
+                    case "stop":
+                        {
+                            connectionEnd = true;
+                        }
+                        break;
+                    case "shutdown":
+                        {
+                            connectionEnd = true;
+                            Environment.Exit(0);
+                        }
+                        break;
+                    case "change":
+                        {
+                            switch (tab[1])
+                            {
+                                case "": break;
+                                default: break;
+                            }
+                        }
+                        break;
+                    case "help":
+                        {
+                            Console.WriteLine("Second Screen IP helpdesk");
+                            Console.WriteLine("List of commands:");
+                            Console.WriteLine("start <ipAddress> <tcpPort> <mode>");
+                            Console.WriteLine("stop");
+                            Console.WriteLine("change <mode>");
+                        }
+                        break;
+                    default: Console.WriteLine("unknown command"); break;
+                }
+            }
         }
 
         static void Main(string[] args)
@@ -42,7 +89,11 @@ namespace Screen_sender
             connectionEnd = false;
             ScreenCapture sc = new ScreenCapture(1.25f);
             byte[] array = sc.captureWithMouse();
-            //Task t1 = sendScreenData();
+            Console.WriteLine(array.Length); //66960 B
+
+
+            //ScreenCapture.turnOffScreen(1000);
+            //Task t1 = sendScreenData(33);
             //Task t2 = serverControl();
             //t1.Wait();
             //t2.Wait();
--- Screen_sender/Screen_sender/ScreenCapture.cs
+++ Screen_sender/Screen_sender/ScreenCapture.cs
@@ -7,12 +7,28 @@ using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using System.Drawing.Imaging;
+using System.Threading;
 
 namespace Screen_sender
 {
+    /// <summary>
+    /// Mirroring -> same screen as primary <span/>
+    /// Extend -> Additional space next to primary<span/>
+    /// Only_second -> Only external screen is active
+    /// </summary>
+    enum ScreenMode
+    {
+        Mirroring = 1, 
+        Extend = 2,
+        Only_second = 3
+    }
+
     class ScreenCapture
     {
-        private float DPI; 
+        private float DPI;
+
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);
 
         public ScreenCapture(float DPI)
         {
@@ -29,6 +45,13 @@ namespace Screen_sender
                     return encoders[j];
             }
             return null;
+        }
+
+        public static void turnOffScreen(int sleep_ms)
+        {
+            SendMessage(0xFFFF, 0x112, 0xF170, 2);
+            Thread.Sleep(sleep_ms);
+            SendMessage(0xFFFF, 0x112, 0xF170, -1);
         }
 
         public void saveScreenshot()
@@ -70,18 +93,13 @@ namespace Screen_sender
             Bitmap cursorBMP = new Bitmap(cur.Size.Width, cur.Size.Height);
 
             // --------------< save image to file >-----------------
-            ImageCodecInfo imageCodecInfo = GetEncoderInfo("image/jpeg");
-            System.Drawing.Imaging.Encoder encoder = System.Drawing.Imaging.Encoder.Quality;
-            EncoderParameter encoderParameter = new EncoderParameter(encoder, 100L);
-            EncoderParameters encoderParameters = new EncoderParameters(1);
-            encoderParameters.Param[0] = encoderParameter;
+
             using (Graphics g = Graphics.FromImage(desktopBMP))
             {
 
                 g.CopyFromScreen(scrBounds.Location, Point.Empty, scrBounds.Size);
                 cur.Draw(g,curBounds);      
             }
-            desktopBMP.Save("screenShot.jpg", imageCodecInfo, encoderParameters);
 
             ImageConverter conv = new ImageConverter();
         
